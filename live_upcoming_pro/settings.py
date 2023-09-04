b989ed62b83fbbcf0139da2442c1d7b9330bb1e5
unknown <palakj935@gmail.com>
2023-09-04 12:13:46 +0530

    Merge remote changes

--- live_upcoming_pro/settings.py
+++ live_upcoming_pro/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-b^6n1he!!-*)3ovc0*(+#3+*#qw5s8%7v$2@avkyd+cgkp141#'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
